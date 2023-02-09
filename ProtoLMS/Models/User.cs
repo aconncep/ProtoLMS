2e33ebab69dd06adbe3ed5c4ce4400a750a6b87f
Austin Cepalia <austin@austincepalia.com>
2023-02-09 15:30:19 -0500

    First attempt at a migration

---
+++ ProtoLMS/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProtoLMS.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
--- ProtoLMS/Pages/Index.cshtml.cs
+++ ProtoLMS/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProtoLMS.Data;
+using ProtoLMS.Models;
 
 namespace ProtoLMS.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<User> Users;
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
+            _db = applicationDbContext;
         }
 
         public void OnGet()
         {
-
+            Users = _db.User;
         }
     }
 }
